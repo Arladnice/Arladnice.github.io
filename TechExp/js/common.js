ed94096a507117981ec090deea48f52b4a4a41a3
arladnice <darlakov@mail.ru>
2020-02-20 00:00:57 +0300

    Update

--- TechExp/js/common.js
+++ TechExp/js/common.js
@@ -132,6 +132,40 @@ $(function() {
 		});
 	});
 
+	$(".mfp-certificate").each(function() {
+		$(this).magnificPopup({
+			delegate: 'a',
+			mainClass: 'mfp-zoom-in',
+			type: 'image',
+			tLoading: '',
+			gallery:{
+				enabled: true,
+			},
+			removalDelay: 300,
+			callbacks: {
+				beforeChange: function() {
+					this.items[0].src = this.items[0].src + '?=' + Math.random();
+				},
+				open: function() {
+					$.magnificPopup.instance.next = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+					}
+					$.magnificPopup.instance.prev = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+					}
+				},
+				imageLoadComplete: function() {
+					var self = this;
+					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+				}
+			}
+		});
+	});
+
 	$(".mouse-icon").click(function() {
 		$("html, body").animate({
 			scrollTop : $(".s-adv").offset().top
@@ -193,7 +227,7 @@ $(function() {
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "/mail.php", //Change
+			url: "../mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
 			th.find(".success").addClass("active");
@@ -248,6 +282,10 @@ $(function() {
 			$(".top").removeClass("active");
 		}
 	});
+
+	$(document).ready(function(){
+			$("input[type='phone']").mask("+7 (999) 999 99-99");
+		});
 
 });
 
