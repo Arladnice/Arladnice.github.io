e1791cdde36350d34c49e64afdc456b8e1581fd5
arladnice <darlakov@mail.ru>
2021-01-28 13:17:56 +0300

    add js

---
+++ 01/js/app.js
@@ -0,0 +1,43 @@
+$(function (params) {
+	var header = $('header'),
+		introH = $('#intro').innerHeight();
+	scrollOffset = 0;
+
+
+  // Fixed Header
+	$(window).on('scroll', function name(params) {
+		scrollOffset = $(this).scrollTop();
+
+		if (scrollOffset >= introH) {
+			header.addClass('header--fixed');
+		} else {
+			header.removeClass('header--fixed');
+		}
+  });
+
+  // Smooth scroll
+
+  $('[data-scroll]').on('click', function(event) {
+    event.preventDefault();
+
+    var $this = $(this),
+        blockId = $(this).data('scroll'),
+        blockOffset = $(blockId).offset().top;
+
+        $('#nav a').removeClass('active');
+        $this.addClass("active");
+
+        $('html, body').animate({
+          scrollTop: blockOffset
+        }, 500);
+  });
+
+  // Nav toggle
+
+  $('#nav-toggle').on('click', function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass('active');
+    $('#nav').toggleClass('active');
+  });
+});
